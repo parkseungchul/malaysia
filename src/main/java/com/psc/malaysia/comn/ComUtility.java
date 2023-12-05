80d6c79d7903ca93815c610010c9554c91df7724
Park Seungchul <parkseungchul@gmail.com>
2023-12-05 19:44:56 +0800

    final

--- src/main/java/com/psc/malaysia/comn/ComUtility.java
+++ src/main/java/com/psc/malaysia/comn/ComUtility.java
@@ -1,26 +1,30 @@
 package com.psc.malaysia.comn;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @Service
 public class ComUtility {
 
+    @Value("${custom.chat-gpt.api-key}")
+    private String apiKey;
+
     public String changeToWeb(String content){
         content = content.replaceAll("\n", "<br>");
         //content = "<p>" + content + "</p>";
-
-        log.debug("==================>"+content);
+        //log.debug("==================>"+content);
         return content;
     }
 
@@ -29,10 +33,25 @@ public class ComUtility {
         return condition;
     }
 
-    public HttpResponse<String> httpRequestBuilder(HttpMethod httpMethod, String apiURL, String apiKey, Map<String, String> headers,  Map<String, String> data) throws IOException, InterruptedException {
+    public <T> T getObjectResponse(HttpResponse<String> response, String tag, Class<T> valueType) throws JsonProcessingException {
+        int statusCode =  response.statusCode();
+        String body = response.body();
+        log.debug(tag + "- Response status code: " + statusCode);
+        log.debug(tag + "- Response body: " + body);
+
+        if(valueType == null){
+            return null;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(body, valueType);
+    }
+
+    public HttpResponse<String> httpRequestBuilder(HttpMethod httpMethod, String apiURL,  Map<String, String> data) throws IOException, InterruptedException {
 
         log.debug("working method: "+httpMethod.toString());
         log.debug("working url: "+apiURL);
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest.Builder builder = HttpRequest.newBuilder();
         if (httpMethod == HttpMethod.GET && data != null && !data.isEmpty()) {
@@ -50,12 +69,15 @@ public class ComUtility {
         } else {
             builder.GET();
         }
+
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("OpenAI-Beta", " assistants=v1");
+        headers.put("Content-Type", "  application/json");
         if (apiKey != null && !apiKey.isEmpty()) {
             builder.header("Authorization", "Bearer " + apiKey);
         }
         headers.forEach(builder::header);
         HttpRequest request = builder.build();
-
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response;
